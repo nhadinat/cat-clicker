3048ae67a3ed993bfc251b5b35357c3c09bad001
nhadinat <nathanhadinata@gmail.com>
2015-09-11 13:38:42 -0700

    Append elements

--- js/script.js
+++ js/script.js
@@ -18,6 +18,10 @@
 // The number of clicks in the cat area should be unique to each
 // cat, and should increment when the cat's picture is clicked.
 
+//////////////////
+// first display all cats with their individual clicker counts
+// then hide all, display when header name clicked.
+
 // Cat Class
 var Cat = function (stringName, stringUrl){
   this.name = stringName;
@@ -26,21 +30,54 @@ var Cat = function (stringName, stringUrl){
   this.count = 0;
 };
 
-Cat.prototype.catBox = function () {
-  // Gather definitions
-  var catName = this.name;
-  var catCountId = this.countId;
-  var catSrc = this.src;
+var cats = [1,2,3,4,5];
+  // Create Cats
+  cats[0] = new Cat('Buttons', 'https://lh3.ggpht.com/nlI91wYNCrjjNy5f-S3CmVehIBM4cprx-JFWOztLk7vFlhYuFR6YnxcT446AvxYg4Ab7M1Fy0twaOCWYcUk=s0#w=640&h=426');
+  cats[1] = new Cat('Chewie', 'https://lh3.ggpht.com/kixazxoJ2ufl3ACj2I85Xsy-Rfog97BM75ZiLaX02KgeYramAEqlEHqPC3rKqdQj4C1VFnXXryadFs1J9A=s0#w=640&h=496');
+  cats[2] = new Cat('Pumpkin', 'http://s3.amazonaws.com/readers/2012/01/25/320pxredcat8727_1.jpg');
+  cats[3] = new Cat('Metoo', 'http://purrfectcatbreeds.com/wp-content/uploads/2014/06/snowshoe-cat3.jpg');
+  cats[4] = new Cat('Tootsie', 'http://vignette3.wikia.nocookie.net/warriorsfanfic/images/f/f1/BrownCat1.jpg/revision/latest?cb=20140613110216');
 
-  // Compile HTML
-  var catContainer =
-    '<div class="catbox"><h1>' + catName + '</h1>' +
-    '<img id="' + catName + '" class="catpic"' +
-    'src="' + catSrc + '">' +
-    '<p id="' + catCountId + '"></p></div>';
-  // Append HTML
-  $('#container').append(catContainer);
-};
+// Let's loop over the numbers in our array
+for (var i = 0; i < cats.length; i++) {
+
+    // This is the number we're on...
+    var cat = cats[i];
+
+    // We're creating a DOM element for the number
+    // Build without jQuery first
+    var elem = document.createElement('div');
+    var header = document.createElement('h1');
+    header.textContent = cat.name;
+    var img = document.createElement('img');
+    img.src = cat.src
+    var counter = document.createElement('p');
+    counter.textContent = cat.count;
+
+    // ... and when we click, alert the value of `num`
+    elem.addEventListener('click', (function(catCopy) {
+        return function() {
+            console.log(catCopy);
+            catCopy.count++;
+            console.log(catCopy.count);
+        };
+    })(cat));
+
+    document.getElementById('container').appendChild(elem);
+    document.getElementById('container').appendChild(header);
+    document.getElementById('container').appendChild(counter);
+    document.getElementById('container').appendChild(img);
+}
+
+/*
+      // Compile HTML
+      var catContainer =
+        '<div class="catbox"><h1>' + cat.name + '</h1>' +
+        '<img id="' + cat.name + '" class="catpic"' +
+        'src="' + cat.src + '">' +
+        '<p id="' + cat.countId + '"></p></div>';
+      // Append HTML
+      $('#container').append(catContainer);
 
 // Clicker Function
 Cat.prototype.clicker = function () {
@@ -65,31 +102,10 @@ chewie.catBox();
 chewie.clicker();
 
 
-/*
-//////////////////
-var cats = [1,2,3];
-
-// Let's loop over the numbers in our array
-for (var i = 0; i < cats.length; i++) {
-
-    // This is the number we're on...
-    var cat = cats[i];
-
-    // We're creating a DOM element for the number
-    var elem = document.createElement('div');
-    elem.textContent = cat;
-
-    // ... and when we click, alert the value of `num`
-    elem.addEventListener('click', (function(catCopy) {
-        return function() {
-            alert(catCopy);
-        };
-    })(cat));
-
-    document.body.appendChild(elem);
-};
 
 
+/////// e.target.id name solution, didn't work because name.count
+// doesnt' mean buttons.count...
 
   // Listen for clicks and increase count
   $(hitId).click(function (e) {
