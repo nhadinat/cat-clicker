103d114479be64ae9d68bf8e8099d920fa3b0bbd
nhadinat <nathanhadinata@gmail.com>
2015-11-22 11:07:23 -0800

    Implement basic knockout observables

--- js/app.js
+++ js/app.js
@@ -1,2 +1,12 @@
+var ViewModel = function () {
+  this.clickCount = ko.observable(0);
+  this.name = ko.observable('Buttons');
+  this.imgSrc = ko.observable('https://lh3.ggpht.com/nlI91wYNCrjjNy5f-S3CmVehIBM4cprx-JFWOztLk7vFlhYuFR6YnxcT446AvxYg4Ab7M1Fy0twaOCWYcUk=s0#w=640&h=426')
+  this.nicknames = ko.observableArray(['Butt-butt', 'Tons', "Lil'B"])
 
-octopus.init();
+  this.incrementCounter = function() {
+    this.clickCount(this.clickCount() + 1)
+  }
+}
+
+ko.applyBindings(new ViewModel());
