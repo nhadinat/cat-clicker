eeb50117db8f6e57fc5e82d434f6e94f82744b6b
nhadinat <nathanhadinata@gmail.com>
2015-11-26 13:27:21 -0800

    Update 2015-11-26T21:27:18.643Z

--- js/app.js
+++ js/app.js
@@ -1,9 +1,43 @@
-var Cat = function () {
+var initialCats = [
+  {
+    clickCount: 0,
+    name: 'Buttons',
+    imgSrc: 'https://lh3.ggpht.com/nlI91wYNCrjjNy5f-S3CmVehIBM4cprx-JFWOztLk7vFlhYuFR6YnxcT446AvxYg4Ab7M1Fy0twaOCWYcUk=s0#w=640&h=426',
+    nicknames: ['Butt-butt', 'Tons', "Buns"]
+  },
+  {
+    clickCount: 0,
+    name: 'Chewie',
+    imgSrc : 'https://lh3.ggpht.com/kixazxoJ2ufl3ACj2I85Xsy-Rfog97BM75ZiLaX02KgeYramAEqlEHqPC3rKqdQj4C1VFnXXryadFs1J9A=s0#w=640&h=496',
+    nicknames: ['Chewbacca']
+  },
+  {
+    clickCount: 0,
+    name: 'Pumpkin',
+    imgSrc : 'http://s3.amazonaws.com/readers/2012/01/25/320pxredcat8727_1.jpg',
+    nicknames: ['Pumps']
+  },
+  {
+    clickCount: 0,
+    name: 'Metoo',
+    imgSrc : 'http://purrfectcatbreeds.com/wp-content/uploads/2014/06/snowshoe-cat3.jpg',
+    nicknames: ['U2']
+  },
+  {
+    clickCount: 0,
+    name: 'Tootsie',
+    imgSrc : 'http://4hdwallpapers.com/wp-content/uploads/2013/04/Funny-Little-Brown-Cat-1024x768.jpg',
+    nicknames: ['Toot-toot']
+  }
+];
+
+
+var Cat = function (data) {
   var self = this;
-  this.clickCount = ko.observable(0);
-  this.name = ko.observable('Buttons');
-  this.imgSrc = ko.observable('https://lh3.ggpht.com/nlI91wYNCrjjNy5f-S3CmVehIBM4cprx-JFWOztLk7vFlhYuFR6YnxcT446AvxYg4Ab7M1Fy0twaOCWYcUk=s0#w=640&h=426');
-  this.nicknames = ko.observableArray(['Butt-butt', 'Tons', "Lil'B"]);
+  this.clickCount = ko.observable(data.clickCount);
+  this.name = ko.observable(data.name);
+  this.imgSrc = ko.observable(data.imgSrc);
+  this.nicknames = ko.observableArray(data.nicknames);
 
   this.title = ko.computed(function(){
     var title;
@@ -34,15 +68,26 @@ var Cat = function () {
 
   this.titleName = ko.computed(function(){
     return self.title() + " " + self.name();
-  })
-}
+  });
+};
 
 var ViewModel = function () {
+  var self = this;
 
-  this.currentCat = ko.observable( new Cat() );
+  this.catList = ko.observableArray([]);
+
+  initialCats.forEach(function(catItem){
+    self.catList.push( new Cat(catItem) );
+  });
+
+  this.currentCat = ko.observable( this.catList()[0] );
 
   this.incrementCounter = function() {
-    this.currentCat().clickCount(this.currentCat().clickCount() + 1);
+    this.clickCount(this.clickCount() + 1);
+  };
+
+  this.setCat = function(clickedCat) {
+    self.currentCat(clickedCat);
   };
 
 };
