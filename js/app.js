0c8d0f154776278a0aed302a18878322d01bba0d
nhadinat <nathanhadinata@gmail.com>
2015-11-26 12:35:01 -0800

    Add titles to cats

--- js/app.js
+++ js/app.js
@@ -1,12 +1,44 @@
-var ViewModel = function () {
+var Cat = function () {
+  var self = this;
   this.clickCount = ko.observable(0);
   this.name = ko.observable('Buttons');
-  this.imgSrc = ko.observable('https://lh3.ggpht.com/nlI91wYNCrjjNy5f-S3CmVehIBM4cprx-JFWOztLk7vFlhYuFR6YnxcT446AvxYg4Ab7M1Fy0twaOCWYcUk=s0#w=640&h=426')
-  this.nicknames = ko.observableArray(['Butt-butt', 'Tons', "Lil'B"])
+  this.imgSrc = ko.observable('https://lh3.ggpht.com/nlI91wYNCrjjNy5f-S3CmVehIBM4cprx-JFWOztLk7vFlhYuFR6YnxcT446AvxYg4Ab7M1Fy0twaOCWYcUk=s0#w=640&h=426');
+  this.nicknames = ko.observableArray(['Butt-butt', 'Tons', "Lil'B"]);
+
+  this.title = ko.computed(function(){
+    var title;
+    var clicks = self.clickCount();
+    if (clicks < 10) {
+      title = "Lil'";
+    } else if (clicks < 50) {
+      title = 'Sir';
+    } else if (clicks < 100) {
+      title = 'Lord';
+    } else if (clicks < 150) {
+      title = 'King';
+    } else if (clicks < 200) {
+      title = 'Emperor';
+    } else if (clicks < 250) {
+      title = 'Ultra';
+    } else {
+      title = 'MegaMecha';
+    }
+    return title;
+  });
+
+  this.titleName = ko.computed(function(){
+    return self.title() + " " + self.name();
+  })
+}
+
+var ViewModel = function () {
+
+  this.currentCat = ko.observable( new Cat() );
 
   this.incrementCounter = function() {
-    this.clickCount(this.clickCount() + 1)
-  }
-}
+    this.currentCat().clickCount(this.currentCat().clickCount() + 1);
+  };
+
+};
 
 ko.applyBindings(new ViewModel());
